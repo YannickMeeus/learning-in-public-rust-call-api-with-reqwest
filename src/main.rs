fe8a925eb7b5ac679850d87afc235715f31baab3
Yannick Meeus <yannick.meeus@gmail.com>
2021-12-30 14:57:07 +0200

    Remove comment detailing structure of response as it's now been ported

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,3 @@
-/*
-A subset of the spec for a pokemon I've already detailed out in my Golang learning
-notes:
-
-type BunchOfPokemon struct {
-	Name    string    `json:"name"`
-	Pokemon []Pokemon `json:"pokemon_entries"`
-}
-
-// A Pokemon Struct to map every pokemon to.
-type Pokemon struct {
-    EntryNo int            `json:"entry_number"`
-    Species PokemonSpecies `json:"pokemon_species"`
-}
-
-// A struct to map our Pokemon's Species which includes it's name
-type PokemonSpecies struct {
-    Name string `json:"name"`
-}
-*/
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug)]
